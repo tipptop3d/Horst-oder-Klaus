8bcfffd06428c19627db0c5bc79fd8c486119695
TippTop <spamtop3d@gmail.com>
2022-03-31 23:19:47 +0200

    comment and line length fix

--- lego/main.py
+++ lego/main.py
@@ -43,7 +43,7 @@ ANGLE_TO_LIFT = 90
 # distance = angle * ANGLE_RATIO
 # Angle needed for given distance = distance divided by angle ratio
 # angle = distance / ANGLE_RATIO
-# Same for speeds (replace distance with distance/time or angle with angle/time)
+# Same for speeds
 SIXTEEN_TEETH_GEAR_DIAMETER = 17.5  # mm
 CIRCUMFERENCE = SIXTEEN_TEETH_GEAR_DIAMETER * math.pi
 ANGLE_RATIO = CIRCUMFERENCE * (1/360)
@@ -55,7 +55,8 @@ PRECISION = 100  # intervals
 
 
 class Plotter:
-    def __init__(self, current_x=X_LEFT_BOUND, current_y=Y_UPPER_BOUND, lifted=False):
+    def __init__(self, current_x=X_LEFT_BOUND,
+                 current_y=Y_UPPER_BOUND, lifted=False):
         self.current_x = current_x
         self.current_y = current_y
 
@@ -93,7 +94,8 @@ class Plotter:
         if x == self.current_x and y == self.current_y:
             return
 
-        if not (X_LEFT_BOUND <= x <= X_RIGHT_BOUND or Y_LOWER_BOUND <= y <= Y_UPPER_BOUND):
+        if not (X_LEFT_BOUND <= x <= X_RIGHT_BOUND
+                or Y_LOWER_BOUND <= y <= Y_UPPER_BOUND):
             raise ValueError('Values out of bounds')
 
         angle_x = (x - self.current_x) / ANGLE_RATIO
@@ -127,33 +129,35 @@ class Plotter:
 
         # calculate timings
         x_speed = (X_MAX_ANGLE_SPEED * ANGLE_RATIO)
-        total_time = X_LENGTH / x_speed
+        total_time = X_LENGTH / x_speed  # t = s / v
         average_time = total_time / PRECISION
 
         # draw loop
         while self.current_x < X_RIGHT_BOUND:
-            print(f'Coords: {self.current_x:.2f} {self.current_y:.2f}')
             x_angle_speed = X_MAX_ANGLE_SPEED
             y_speed = fp.evaluate(self.current_x)
             y_angle_speed = y_speed / ANGLE_RATIO
-            abs_y_angle_speed = abs(y_angle_speed)
 
+            # speed factor is the ratio of y to y_max
             speed_factor = 1.0
             # if y-speed exceeds, slow down x-motor to retain ratio
-            # speed factor is the ratio of y to y_max
-            # dividing it with our x-speed gives the lowered x-speed retaining ratio
-            if abs_y_angle_speed > Y_MAX_ANGLE_SPEED:
-                speed_factor = abs_y_angle_speed / Y_MAX_ANGLE_SPEED
+            if abs(y_angle_speed) > Y_MAX_ANGLE_SPEED:
+                speed_factor = abs(y_angle_speed / Y_MAX_ANGLE_SPEED)
+                # respect orientation
                 y_angle_speed = math.copysign(Y_MAX_ANGLE_SPEED, y_speed)
+                # dividing speed_factor with x-speed
+                # gives the lowered x-speed, retaining ratio
                 x_angle_speed /= speed_factor
 
             motor_y.run(y_angle_speed)
             motor_x.run(x_angle_speed)
 
-            # average time multiplied with speed factor gives the time
+            # average time multiplied with speed factor
+            # gives the actual time for the current speeds
             time_spent = average_time * speed_factor
 
-            # distance = velocity * time | s = v · t
+            # needed for loop
+            # s = v · t
             self.current_y += (y_angle_speed * ANGLE_RATIO) * time_spent
             self.current_x += (x_angle_speed * ANGLE_RATIO) * time_spent
 
